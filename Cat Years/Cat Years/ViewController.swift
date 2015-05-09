2b9f8aa352bbfa508831d13bdce1995608d8df01
JohnDuxbury <JohnDuxbury@users.noreply.github.com>
2015-05-09 11:06:26 +0100

    Cat Age Calculator

--- Cat Years/Cat Years/ViewController.swift
+++ Cat Years/Cat Years/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageDisplay: UILabel!
+    @IBOutlet weak var ageEntry: UITextField!
+    @IBAction func ageCalculate(sender: AnyObject) {
+        
+        var entryAge = ageEntry.text.toInt()
+        
+        if entryAge != nil {
+        
+            var catYears = entryAge! * 7
+        
+            ageDisplay.text="Your cat is \(catYears) in cat years"
+        
+        println(catYears)
+        }else{
+            ageDisplay.text="Please enter a whole number!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
