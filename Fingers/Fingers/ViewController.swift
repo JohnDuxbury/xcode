d898200343dd6d884b099e806eaec5d46b03d35d
JohnDuxbury <JohnDuxbury@users.noreply.github.com>
2015-05-09 15:18:01 +0100

    Fingers Guessing Game

--- Fingers/Fingers/ViewController.swift
+++ Fingers/Fingers/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var guessResult: UILabel!
+    
+    @IBOutlet weak var guessEntry: UITextField!
 
+    @IBAction func guessButton(sender: AnyObject) {
+        
+        var randomNumber = arc4random_uniform(6)
+        
+        var guessInt = guessEntry.text.toInt()
+        
+        guessResult.text=toString(randomNumber)
+        
+        if guessInt != nil {
+            
+            if Int(randomNumber) == guessInt
+            {
+                guessResult.text="Good Guess!"
+            } else
+            {
+                guessResult.text="Sorry - the correct number was \(randomNumber)!"
+            }
+            
+        }else{
+            
+            guessResult.text="Please enter a number between 1 - 5!"
+            
+        }
+
+    }
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
